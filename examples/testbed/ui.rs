8ede1484441a331e486c547a90cee2ce1d579ca8
Noah Callaway <ncallaway@gmail.com>
2020-08-27 18:14:49 -0700

    Issue #5: Rename messages -> notes

--- examples/testbed/ui.rs
+++ examples/testbed/ui.rs
@@ -2,10 +2,8 @@ use bevy::prelude::*;
 
 use smallvec::SmallVec;
 
-use super::game::Message;
+use super::game::Note;
 
-const STAGE_UI_BEFORE: &str = "ui_before";
-const STAGE_UI_AFTER: &str = "ui_after";
 const UI_BACKGROUND: Color = Color::rgba(0.0, 0.0, 0.0, 0.9);
 
 struct ContainerEntity(Entity);
@@ -16,11 +14,11 @@ struct ButtonMaterials {
     pressed: Handle<ColorMaterial>,
 }
 
-struct MessageDisplay {
+struct NoteDisplay {
     ordinal: u8,
 }
 
-struct MessageContainer;
+struct NoteContainer;
 
 impl FromResources for ButtonMaterials {
     fn from_resources(resources: &Resources) -> Self {
@@ -33,16 +31,29 @@ impl FromResources for ButtonMaterials {
     }
 }
 
+pub mod stages {
+    // const STAGE_UI_BEFORE: &str = "ui_before";
+    // const STAGE_UI_AFTER: &str = "ui_after";
+
+    pub const USER_EVENTS: &str = "user_events";
+    pub const DOMAIN_EVENTS: &str = "domain_events";
+
+    pub const DOMAIN_SYNC: &str = "domain_sync";
+    pub const VISUAL_SYNC: &str = "visual_sync";
+}
+
 pub fn build(app: &mut AppBuilder) {
     app.init_resource::<ButtonMaterials>()
         .init_resource::<Handle<Font>>()
         .add_resource(ContainerEntity(Entity::new()))
         .add_startup_system(setup_ui.system())
-        .add_stage_before(stage::UPDATE, STAGE_UI_BEFORE)
-        .add_stage_after(stage::UPDATE, STAGE_UI_AFTER)
-        .add_system_to_stage(STAGE_UI_BEFORE, button_hover_system.system())
-        .add_system(message_display_sync_system.system())
-        .add_system_to_stage(STAGE_UI_AFTER, message_display_ordering_system.system());
+        .add_stage_before(stage::UPDATE, stages::USER_EVENTS)
+        .add_stage_after(stage::UPDATE, stages::DOMAIN_EVENTS)
+        .add_stage_after(stages::DOMAIN_EVENTS, stages::DOMAIN_SYNC)
+        .add_stage_after(stages::DOMAIN_SYNC, stages::VISUAL_SYNC)
+        .add_system_to_stage(stages::USER_EVENTS, button_hover_system.system())
+        .add_system(note_display_sync_system.system())
+        .add_system_to_stage(stages::VISUAL_SYNC, note_display_ordering_system.system());
 }
 
 fn setup_ui(
@@ -95,7 +106,7 @@ fn setup_ui(
                     // button label
                     parent.spawn(TextComponents {
                         text: Text {
-                            value: "Send a message".to_string(),
+                            value: "Send a note".to_string(),
                             font: *font_handle,
                             style: TextStyle {
                                 font_size: 12.0,
@@ -105,7 +116,7 @@ fn setup_ui(
                         ..Default::default()
                     });
                 })
-                // messages box
+                // notes box
                 .spawn(NodeComponents {
                     style: Style {
                         size: Size::new(Val::Percent(100.0), Val::Auto),
@@ -131,14 +142,14 @@ fn setup_ui(
                                 ..Default::default()
                             },
                         )
-                        .with(MessageContainer)
+                        .with(NoteContainer)
                         .spawn(TextComponents {
                             style: Style {
                                 align_self: AlignSelf::Center,
                                 ..Default::default()
                             },
                             text: Text {
-                                value: "MESSAGES ".to_string(),
+                                value: "NOTES ".to_string(),
                                 font: *font_handle,
                                 style: TextStyle {
                                     font_size: 24.0,
@@ -170,35 +181,35 @@ fn button_hover_system(
     }
 }
 
-fn message_display_sync_system(
+fn note_display_sync_system(
     mut commands: Commands,
     font_handle: Res<Handle<Font>>,
-    mut messages: Query<&Message>,
-    mut display_containers: Query<(Entity, &MessageContainer)>,
-    mut message_displays: Query<(Entity, &mut MessageDisplay, &mut Text)>,
+    mut notes: Query<&Note>,
+    mut display_containers: Query<(Entity, &NoteContainer)>,
+    mut note_displays: Query<(Entity, &mut NoteDisplay, &mut Text)>,
 ) {
-    let mut display_borrow = message_displays.iter();
+    let mut display_borrow = note_displays.iter();
     let mut display_iter = display_borrow.into_iter();
 
-    for msg in &mut messages.iter() {
+    for note in &mut notes.iter() {
         let has_display = display_iter.next();
 
         match has_display {
             Some((_, mut display, mut text)) => {
                 // oh no! A string allocation in a system loop! That's not great. I don't care
                 // about the performance of this, but please don't copy this somewhere real.
-                let desired_text = format!("[{}] {}", msg.from, msg.message);
-                if text.value != desired_text || display.ordinal != msg.ordinal {
-                    text.value = format!("[{}] {}", msg.from, msg.message);
-                    display.ordinal = msg.ordinal;
+                let desired_text = format!("[{}] {}", note.from, note.note);
+                if text.value != desired_text || display.ordinal != note.ordinal {
+                    text.value = format!("[{}] {}", note.from, note.note);
+                    display.ordinal = note.ordinal;
                 }
             }
-            None => spawn_message_display(
+            None => spawn_note_display(
                 &mut commands,
                 *font_handle,
                 &mut display_containers,
-                format!("[{}] {}", msg.from, msg.message),
-                msg.ordinal,
+                format!("[{}] {}", note.from, note.note),
+                note.ordinal,
             ),
         }
     }
@@ -209,11 +220,11 @@ fn message_display_sync_system(
     }
 }
 
-fn message_display_ordering_system(
-    mut containers: Query<(Entity, &MessageContainer, &mut Children)>,
-    mut message_display: Query<(Entity, &mut MessageDisplay)>,
+fn note_display_ordering_system(
+    mut containers: Query<(Entity, &NoteContainer, &mut Children)>,
+    mut note_display: Query<(Entity, &mut NoteDisplay)>,
 ) {
-    // get the children of the message container
+    // get the children of the note container
     let mut container_borrow = containers.iter();
     let mut container_children = match container_borrow.into_iter().next() {
         Some((_, _, c)) => c,
@@ -226,7 +237,7 @@ fn message_display_ordering_system(
     let mut needs_reorder = false;
 
     for child in container_children.iter() {
-        let d = message_display.get::<MessageDisplay>(*child).unwrap();
+        let d = note_display.get::<NoteDisplay>(*child).unwrap();
 
         if let Some(prior) = last {
             if d.ordinal > prior {
@@ -238,8 +249,8 @@ fn message_display_ordering_system(
     }
 
     if needs_reorder {
-        let mut display_borrow = message_display.iter();
-        let mut sorted_displays: Vec<(Entity, Mut<MessageDisplay>)> =
+        let mut display_borrow = note_display.iter();
+        let mut sorted_displays: Vec<(Entity, Mut<NoteDisplay>)> =
             display_borrow.into_iter().collect();
 
         sorted_displays.sort_by(|(_, a), (_, b)| b.ordinal.cmp(&a.ordinal));
@@ -249,26 +260,26 @@ fn message_display_ordering_system(
     }
 }
 
-fn spawn_message_display(
+fn spawn_note_display(
     commands: &mut Commands,
     font_handle: Handle<Font>,
-    display_containers: &mut Query<(Entity, &MessageContainer)>,
+    display_containers: &mut Query<(Entity, &NoteContainer)>,
     value: String,
     ordinal: u8,
 ) {
-    // have to have a message container, or we die, sorry.
+    // have to have a notes container, or we die, sorry.
     let (container_entity, _) = display_containers.iter().into_iter().next().unwrap();
-    spawn_message_display_with_entity(commands, &font_handle, container_entity, value, ordinal);
+    spawn_note_display_with_entity(commands, &font_handle, container_entity, value, ordinal);
 }
 
-fn spawn_message_display_with_entity(
+fn spawn_note_display_with_entity(
     commands: &mut Commands,
     font_handle: &Handle<Font>,
     container_entity: Entity,
     value: String,
     ordinal: u8,
 ) {
-    let md = MessageDisplay { ordinal };
+    let md = NoteDisplay { ordinal };
 
     let e = Entity::new();
     commands
